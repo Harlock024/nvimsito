c333c03ede9cd50e54249b7afd84520954026094
harlock024 <hadith024@gmail.com>
2025-07-09 01:28:25 -0600

    nvimsito config

--- init.lua
+++ init.lua
@@ -10,16 +10,23 @@ require('lsp').setup()
 
 
 vim.diagnostic.config({
-  virtual_text = true,   -- muestra el error justo al lado del código
-  signs = true,          -- muestra iconos en la columna de la izquierda (sign column)
-  underline = true,      -- subraya el texto con error
-  update_in_insert = true, -- actualiza diagnósticos mientras estás insertando texto
+  virtual_text = true,   
+  signs = true,          
+  underline = true,      
+  update_in_insert = true, 
   severity_sort = true,
 })
 
 vim.cmd([[colorscheme lackluster-hack]])
 vim.opt.clipboard:append("unnamedplus")
 
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.fillchars = {eob = " "}
 
 
 
